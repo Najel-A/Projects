40657d07d8c30540c427e16dbfa9d2a97d463f3b
Najel-A <59577073+Najel-A@users.noreply.github.com>
2020-04-04 02:24:09 -0700

    Updated UnitCircle Folder

--- Projects/UnitCircle/README.txt
+++ Projects/UnitCircle/README.txt
@@ -1,3 +1,2 @@
-This programs displays a circle with dashed lines going from the origin of a graph (0,0) to each of the coordinates of the unit circle using Matplotlib.
+This programs displays a circle with dashed lines going from the origin of a graph (0,0) to each of the coordinates of the unit circle using Matplotlib. It also includes a legend which tells you the coordinates in Radians as well.
 
-*In Progress*
--- Projects/UnitCircle/UnitCircle.py
+++ Projects/UnitCircle/UnitCircle.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import math
+from matplotlib.legend import Legend
 
 
 # Functions
@@ -11,11 +12,18 @@ def create_circle():
 def show_shape(patch):
     ax = plt.gca()
     ax.add_patch(patch)
+    plt.title("Unit Circle Graph",fontsize="24",color="white")
     plt.axis('scaled')
+    ax.set_facecolor((1.0, .47, .42))
+    #plt.axis()
+    plt.ylabel("Y-Coordinates",fontsize="24")
+    plt.xlabel("X-Coordinates",fontsize="24")
     plt.show()
 
 
-plt.title("Unit Circle")
+# Background Color
+plt.figure().patch.set_facecolor("magenta")
+
 # Horizontal Line
 x = [-1, 1]
 y = [0, 0]
@@ -47,6 +55,7 @@ point12 = [1 / 2, -math.sqrt(3) / 2]
 # Quadrant 1
 line1x = [origin[0], point1[0]]
 line1y = [origin[1], point1[1]]
+first_line = [line1x, line1y]
 
 line2x = [origin[0], point2[0]]
 line2y = [origin[1], point2[1]]
@@ -98,7 +107,63 @@ plt.plot(line10x, line10y, color="yellow", ls="dashed")
 plt.plot(line11x, line11y, color="yellow", ls="dashed")
 plt.plot(line12x, line12y, color="yellow", ls="dashed")
 
+# Legend
+plt.plot([point1[0]], label="            KEY     ")
+plt.plot([point1[0]], label=" ")
 
+plt.plot([point1[0]], label="Quadrant 1", color="blue")
+plt.plot([point4[0]], label="Quadrant 2", color="red")
+plt.plot([point7[0]], label="Quadrant 3", color="green")
+plt.plot([point10[0]], label="Quadrant 4", color="yellow")
 
+plt.plot([point1[0]], label=" ")
+
+plt.plot([point1[0]], label="π/6 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="π/4 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="π/3 at (√3/2, 1/2)")
+
+plt.plot([point1[0]], label=" ")
+
+plt.plot([point1[0]], label="2π/3 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="3π/4 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="5π/6 at (√3/2, 1/2)")
+
+plt.plot([point1[0]], label=" ")
+
+plt.plot([point1[0]], label="7π/6 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="5π/4 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="4π/3 at (√3/2, 1/2)")
+
+plt.plot([point1[0]], label=" ")
+
+plt.plot([point1[0]], label="5π/3 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="7π/4 at (√3/2, 1/2)")
+plt.plot([point1[0]], label="11π/6 at (√3/2, 1/2)")
+
+legend = plt.legend(bbox_to_anchor=(1, 1), loc="upper left", handlelength=0, handletextpad=-.2, facecolor="black",
+                    framealpha=1)
+color_lines = ["white", "black", "blue", "red", "green", "yellow", "black", "blue", "blue", "blue", "black", "red",
+               "red", "red", "black", "green", "green", "green", "black", "yellow", "yellow", "yellow"]
+for n, text in enumerate(legend.texts):
+    print(n, text)
+    text.set_color(color_lines[n])
+
+# Points Plot
+plt.plot(point1[0], point1[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point2[0], point2[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point3[0], point3[1], "ro", markersize=4.5, color="cyan")
+
+plt.plot(point4[0], point4[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point5[0], point5[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point6[0], point6[1], "ro", markersize=4.5, color="cyan")
+
+plt.plot(point7[0], point7[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point8[0], point8[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point9[0], point9[1], "ro", markersize=4.5, color="cyan")
+
+plt.plot(point10[0], point10[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point11[0], point11[1], "ro", markersize=4.5, color="cyan")
+plt.plot(point12[0], point12[1], "ro", markersize=4.5, color="cyan")
+
+# Draw Graph
 show_shape(create_circle())
-plt.show()
